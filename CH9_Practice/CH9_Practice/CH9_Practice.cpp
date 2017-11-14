035774f5cc0dcf6f31145da71ff658fdd65035b7
claytonjwong <claytonjwong@gmail.com>
2017-11-14 10:26:44 -0800

    9.2 Total Path Count ( base case bug fix )

--- CH9_Practice/CH9_Practice/CH9_Practice.cpp
+++ CH9_Practice/CH9_Practice/CH9_Practice.cpp
@@ -106,13 +106,13 @@ public:
     class SolutionREC {
     public:
         int pathCount(int m, int n){
-            if (m<1 || n<1) return 0;
             return helper(m-1,n-1);
             
         }
     private:
         int helper(int m, int n){
-            if (m==0 && n==0) return 1;
+            if (m==0 && n==0) return 0;
+            if (m==0 || n==0) return 1;
             if (m==0) return helper(0,n-1);
             if (n==0) return helper(m-1,0);
             return helper(m-1,n) + helper(m, n-1);
@@ -123,11 +123,11 @@ public:
     class SolutionDP {
     public:
         int pathCount(int m, int n){
-            if (m<1 || n<1) return 0;
+            if (m==0 && n==0) return 0;
             vector<vector<int>> dp(m,vector<int>(n,0));
-            for (int i=0; i<m; ++i)
+            for (int i=1; i<m; ++i)
                 dp[i][0]=1;
-            for (int j=0; j<n; ++j)
+            for (int j=1; j<n; ++j)
                 dp[0][j]=1;
             for (int i=1; i<m; ++i)
                 for (int j=1; j<n; ++j)
@@ -136,6 +136,7 @@ public:
         }
     };
 };
+
 
 
 int main(int argc, const char * argv[]) {
